b1c5ddce0425d045084e753ff9ee4a9137c13827
Pavlo Hodysh <gaarasmail@gmail.com>
2020-10-31 23:57:38 +0200

    feature: used CliWrap library to excute commands, set the default output to Console stream of parent process

--- src/Silent.Tool.Hexagonal.Cli/Commands/InitializeCommand.cs
+++ src/Silent.Tool.Hexagonal.Cli/Commands/InitializeCommand.cs
@@ -1,5 +1,6 @@
 ﻿using CliFx;
 using CliFx.Attributes;
+using CliWrap;
 using Microsoft.Extensions.Options;
 using Silent.Tool.Hexagonal.Cli.Infrastructure.Options;
 using System;
@@ -20,24 +21,38 @@ namespace Silent.Tool.Hexagonal.Cli
         [CommandParameter(0)]
         public string ProjectName { get; set; }
 
-        public ValueTask ExecuteAsync(IConsole console)
+        public async ValueTask ExecuteAsync(IConsole console)
         {
-            var task = Task.FromResult(HandleIniCommand(_options));
-            return new ValueTask(task);
+            await HandleIniCommand(_options);
         }
 
-        private bool HandleIniCommand(IOptions<GeneralOptions> options)
+        private async Task<bool> HandleIniCommand(IOptions<GeneralOptions> options)
         {
-            string solutionCommand = $"dotnet new sln --name \"{ProjectName}\" --output \"{ProjectName}\"";
+            var consoleOutputTarget = PipeTarget.ToStream(Console.OpenStandardOutput());
+
+            // dotnet new sln --name \"{ProjectName}\" --output \"{ProjectName}\"
+            var domainCommand = CliWrap.Cli.Wrap("dotnet")
+                .WithStandardOutputPipe(consoleOutputTarget)
+                .WithArguments(args => args
+                    .Add("new")
+                    .Add("sln")
+                    .Add("--name")
+                    .Add(ProjectName)
+                    .Add("--output")
+                    .Add(ProjectName)
+                    .Add("--framework"));
+
+            var domainCommandResult = await domainCommand.ExecuteAsync();
 
             var successfull = true;
+
             successfull &= StringExtensions.CreateDirectorySafely($"{ProjectName}/{options.Value.Folders.ClientsFolder}");
             successfull &= StringExtensions.CreateDirectorySafely($"{ProjectName}/{options.Value.Folders.DocsFolder}");
             successfull &= StringExtensions.CreateDirectorySafely($"{ProjectName}/{options.Value.Folders.SamplesFolder}");
             successfull &= StringExtensions.CreateDirectorySafely($"{ProjectName}/src/{options.Value.Folders.ServicesFolder}");
             successfull &= StringExtensions.CreateDirectorySafely($"{ProjectName}/src/{options.Value.Folders.WebAppsFolder}");
             successfull &= StringExtensions.CreateDirectorySafely($"{ProjectName}/test");
-            successfull &= StringExtensions.RunInCommandPrompt(solutionCommand);
+            successfull &= domainCommandResult.ExitCode == 0;
 
             return successfull;
         }
